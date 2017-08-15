96878377131266735c502d64e39773fcdaa39db7
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-15 22:24:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHtml='hello';
 var img=document.getElementById('madi');
+var marginLeft=0;
 img.onclick=function(){
    var interval=setInterval(moveRight,100);
 };
-var marginLeft=0;
+
 function moveRight(){
-    marginLeft=marginleft+10;
-    img.style.marginLeft=marginleft+'px';
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
 }
