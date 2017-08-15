6bde364947343af3e3d6a3d062d68b8777050a29
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-15 22:16:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHtml='hello';
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
