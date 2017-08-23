5af7516da74e732e941ecada60d94347a8c9c040
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-23 22:19:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var congif={
+    user:'nandhiniks92',
+    database:'nandhiniks92',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_Password
+    
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
-
+var pool=new Pool(config);
+app.get('/test.db',function(req,res){
+   pool.query('select * from test',function(err,result)
+   {
+       if(err)
+       {
+           res.status(500).send(err.toString());
+           
+       }else
+       {
+           res.send(JSON.stringfy(result));
+       }
+   }) ;
+});
 /*var articles={
      'article-one':{
         title:'My article one',
