5b92960da708939c0831dec054790ae759572ebd
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-23 22:22:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var pool=new Pool(config);
-app.get('/test.db',function(req,res){
+app.get('/test-db',function(req,res){
    pool.query('select * from test',function(err,result)
    {
        if(err)
