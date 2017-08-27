0fe37144e3a482e03c04fb438038c10c8f9eb782
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-27 22:35:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/articles/:articleNames',function(req,res){
           }else
           {
               var articleData=result.rows[0];
-              res.send(createTemplate(articleData));
+             res.send(createTemplate(articleData));
           }
        }
        
