205151b1735310a963cccaf0a077b31acd6ca561
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-27 22:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,7 @@ app.get('/articles/:articleNames',function(req,res){
        if(err)
        {
            res.status(500).send(err.toString());
-       }else
-       {
+       }else{
           if(result.rows.length===0)
           {
               res.status(404).send('Article no found!');
