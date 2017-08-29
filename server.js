131467f46e16ec46bcf28a8bd4bd8bddedf7e9c1
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-29 22:17:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,13 +103,13 @@ app.get('/articles/:articleNames',function(req,res){
        
     
 });
-function hash(input){
-    var salt='this is secrete password';
+function hash(input,salt){
+   
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString(hex);
 }
 app.get('/hash/:input',function(req,res){
-   var hashedString=hash(req.params.input) ;
+   var hashedString=hash(req.params.input,'this is secrete password') ;
    res.send(hashedString);
    
    
