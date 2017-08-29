c3c22b967c09cce8e11845ea0bb1c76215c3f2c3
NandhiniKS <30695362+NandhiniKS@users.noreply.github.com>
2017-08-29 23:02:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user:'nandhiniks92',
     database:'nandhiniks92',
@@ -15,7 +16,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 /*var articles={
      'article-one':{
         title:'My article one',
@@ -94,6 +95,23 @@ console.log('in');
    
    
 });
+
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('insert into "users"(username,password)values($1,$2)',[username,dbString],function(err,result){
+        if(err)
+        {
+               res.status(500).send(err.toString());
+       }else{
+           res.send('User Successfully Create'+username);
+        }
+    });
+});
+
+
 
 app.get('/articles/:articleNames',function(req,res){
    //  res.sendFile(path.join(__dirname,'ui', 'activity_one.html'));
